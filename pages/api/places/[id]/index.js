b7fb3c2dc7b17867ec890175b8702ac58721a387
Nikolas <nik.steppenwolf@web.de>
2025-03-26 12:24:56 +0100

    Routen auf Switch umgestellt

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -5,16 +5,15 @@ export default async function handler(request, response) {
   dbConnect();
   const { id } = request.query;
 
-  //const place = places.find((place) => place.id === id);
-
-  if (request.method === "GET") {
-    try {
-      const place = await Places.findById(id);
-      response.status(200).json(place);
-      return;
-    } catch (error) {
-      response.status(404).json({ status: error.message });
-      return;
-    }
+  switch (request.method) {
+    case "GET":
+      try {
+        const place = await Places.findById(id);
+        response.status(200).json(place);
+        break;
+      } catch (error) {
+        response.status(404).json({ status: error.message });
+        break;
+      }
   }
 }
