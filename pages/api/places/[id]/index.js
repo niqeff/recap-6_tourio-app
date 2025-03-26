e13497ea4f570739e5f663658d1053f563ed4800
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-03-26 11:42:30 +0100

    add successfully id connection

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,14 +1,20 @@
-import { places } from "../../../../lib/db.js";
+import dbConnect from "@/db/connect.js";
+import Places from "@/db/models/Places.js";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  dbConnect();
   const { id } = request.query;
 
-  const place = places.find((place) => place.id === id);
+  //const place = places.find((place) => place.id === id);
 
-  if (!place) {
-    response.status(404).json({ status: "Not found" });
-    return;
+  if (request.method === "GET") {
+    try {
+      const place = await Places.findById(id);
+      response.status(200).json(place);
+      return;
+    } catch (error) {
+      response.status(404).json({ status: error.message });
+      return;
+    }
   }
-
-  response.status(200).json(place);
 }
